7a316defbf1902996561d847af1ab3cd87bb0d23
Pranay <146086558+Pranay2874@users.noreply.github.com>
2025-02-05 17:39:24 +0530

    Updated CORS for Vercel deployment

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",  
         "https://makeafrnd2.onrender.com",  
-        "https://makeafrnd-project.vercel.app"  
+        "https://makeafrnd-project.vercel.app" 
     ], 
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
